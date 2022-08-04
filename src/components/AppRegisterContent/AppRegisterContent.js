4e8c3cf4ef34292aa0fd7fc6ed3c1f7d3504c1a1
Rico Guinanao <83864808+coricss@users.noreply.github.com>
2022-08-04 21:26:52 +0800

    fix reg styles

--- src/components/AppRegisterContent/AppRegisterContent.js
+++ src/components/AppRegisterContent/AppRegisterContent.js
@@ -11,28 +11,28 @@ const AppRegisterContent = () => (
             <h1 className='text-danger'>Register Here</h1>
           </div>
           <hr className='text-white'></hr>
-          <div className='input-container mt-4'>
-            <div className="form-group w-50 mb-3">
+          <center className='mt-4'>
+            <div className="form-group mb-3">
               <input type="text" className="form-control" id="username" placeholder="Username" />
             </div>
-            <div className="form-group w-50 mb-3">
+            <div className="form-group mb-3">
               <input type="email" className="form-control" id="email" placeholder="Email" />
             </div>
-            <div className="form-group w-50 mb-3">
+            <div className="form-group mb-3">
               <input type="password" className="form-control" id="pwd" placeholder="Password" />
             </div>
-            <div className="form-group w-50 mb-3">
+            <div className="form-group mb-3">
               <input type="password" className="form-control" id="pwd2" placeholder="Confirm password" />
             </div>
-            <div className="w-50 mb-3 text-white">
+            <div className="mb-3 text-white">
               <input type="checkbox" className="form-check-input me-2" id="checkbox" placeholder="Confirm password" />
               <label htmlFor='checkbox'>I am over 18 years old</label>
             </div>
             <div className="form-group w-100 mb-3">
-              <button className='btn btn-danger btn-lg w-50' >Get started</button>
+              <button className='btn btn-danger btn-lg' >Get started</button>
             </div>
             <small className='text-white'>Do you already have an account? <a href='/login' className='text-danger fw-bold text-decoration-none'>Login</a></small>
-          </div>
+          </center>
         </form>
       </div>
     </div>
